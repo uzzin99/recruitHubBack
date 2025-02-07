6af2a1aaa0a98facb32a2f389db138d6e2062b8e
uzzin99 <106142771+uzzin99@users.noreply.github.com>
2025-02-07 10:32:11 +0900

    Initial commit

---
+++ src/main/java/com/jangyujin/recruitHubBack/RecruitHubBackApplication.java
@@ -0,0 +1,13 @@
+package com.jangyujin.recruitHubBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitHubBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitHubBackApplication.class, args);
+	}
+
+}
